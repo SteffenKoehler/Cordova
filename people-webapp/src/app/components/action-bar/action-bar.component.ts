227951c01909b93395b4b04b338a7dc0c4f40fc0
Steffen Köhler <steffen.koehler@sovanta.com>
2017-07-05 13:51:36 +0200

    updated action bar title

--- people-webapp/src/app/components/action-bar/action-bar.component.ts
+++ people-webapp/src/app/components/action-bar/action-bar.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subscription} from 'rxjs/Subscription';
+import {NavigationService} from '../../shared/navigation/navigation.service';
+
 
 @Component({
   selector: 'app-action-bar',
   templateUrl: './action-bar.component.html',
   styleUrls: ['./action-bar.component.scss']
 })
-export class ActionBarComponent implements OnInit {
+export class ActionBarComponent implements OnInit, OnDestroy {
+    selectedView: number;
+    subscription: Subscription;
 
-  constructor() { }
+    constructor(
+      private navigationService: NavigationService
+    ) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.getSelectedNavigation();
+    }
+
+    getSelectedNavigation () {
+        this.subscription = this.navigationService.navItem
+            .subscribe(
+                item => this.selectedView = item,
+                error => console.log('Subscription error', error)
+            )
+    }
+
+    ngOnDestroy() {
+        // prevent memory leak when component is destroyed
+        this.subscription.unsubscribe();
+    }
 
 }
