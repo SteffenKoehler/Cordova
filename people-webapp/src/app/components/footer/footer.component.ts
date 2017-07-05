2850ee3cb991aa2d3e53d338407e5902a049af97
Steffen Köhler <steffen.koehler@sovanta.com>
2017-07-05 14:14:54 +0200

    changing footer icons

--- people-webapp/src/app/components/footer/footer.component.ts
+++ people-webapp/src/app/components/footer/footer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {NavigationService} from '../../shared/navigation/navigation.service';
+import { NavigationService } from '../../shared/navigation/navigation.service';
 
 @Component({
     selector: 'app-footer',
@@ -7,14 +7,40 @@ import {NavigationService} from '../../shared/navigation/navigation.service';
     styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-    item = 1;
+    selectedView: number;
+    srcMapIcon: string;
+    srcListIcon: string;
+    srcSettingsIcon: string;
     constructor(private _navigationService: NavigationService) { }
 
     ngOnInit() {
+        this.selectedView = 1;
+        this.setSrcForIcons();
     }
 
     selectedNavItem(item: number) {
         console.log('selected nav item ' + item);
+        this.selectedView = item;
+        this.setSrcForIcons();
         this._navigationService.changeNav(item);
     }
+
+    setSrcForIcons() {
+        this.srcMapIcon = '../../../assets/images/map.svg';
+        this.srcListIcon = '../../../assets/images/contact.svg';
+        this.srcSettingsIcon = '../../../assets/images/setting.svg';
+
+        switch (this.selectedView) {
+            case 1:
+                this.srcMapIcon = '../../../assets/images/map_orange.svg';
+                break;
+            case 2:
+                this.srcListIcon = '../../../assets/images/contact_orange.svg';
+                break;
+            case 3:
+                this.srcSettingsIcon = '../../../assets/images/setting_orange.svg';
+                break;
+        }
+    }
 }
+
