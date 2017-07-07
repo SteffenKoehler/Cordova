d0d3ba27787ebc26ce789c770eb8f6e6a2f5974b
Steffen Köhler <steffen.koehler@sovanta.com>
2017-07-07 10:50:05 +0200

    created userData storage

---
+++ people-webapp/src/app/providers/userData/userData.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by steffen.koehler on 07.07.17.
+ */
+
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class UserData {
+    public storage: any;
+
+    constructor() {}
+}
